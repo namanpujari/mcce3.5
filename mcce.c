843d9f82187ea70ab7d7d51896c7e23a480adfc4
SALAH SALAH <salah@master.cl.cuny.edu>
2016-07-31 23:41:04 -0400

    update

--- mcce.c
+++ mcce.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "mcce.h"
-//#include <mpi.h>
+#include <time.h>
 
 void welcome();
+#define LEN 150
 
 int main(int argc, char *argv[])
 {
@@ -68,54 +69,53 @@ int main(int argc, char *argv[])
 }
 
 void welcome()
-{  printf("===========================================================\n");
-   printf("<<< MCCE Multi-Conformation Continuum Electrostatics >>>   \n");
-   printf(" Marilyn Gunner's Lab at City College of New York, 2005    \n");
-   printf("-----------------------------------------------------------\n");
-   printf("Version:        2.5.1                                      \n");
-   printf("MCCE Home Page: http://www.sci.ccny.cuny.edu/~gunner/mcce  \n");
-   printf("Support:        gunner@sci.ccny.cuny.edu                   \n");
-   printf("Developed by:   Junjun Mao, Yifan Song, Marilyn Gunner     \n");
-   printf("Reference MCCE: If you publish data calculated with MCCE,  \n");
-   printf("                you need to cite papers suggested in MCCE  \n");
-   printf("                Home Page.                                 \n");
-   printf("===========================================================\n\n");
+{ 
+   printf(" _________________________MCCE 3.5____________________________\n");
+   printf("|	   						      |\n");
+   printf("|    MCCE (Multi-Conformation Continuum Electrostatics)       |\n");
+   printf("|	is a program developed at Marilyn Gunner's lab.	      |\n");
+   printf("|	MCCE is a biophysics simulation program combining     |\n");
+   printf("|	continuum electrostatics and molecular mechanics.     |\n");
+   printf("|	In this program, the protein side chain motions are   |\n");
+   printf("|	simulated explicitly while the dielectric effect of   |\n");
+   printf("|	solvent and bulk protein material is modeled by	      |\n");
+   printf("|	continuum electrostatics.			      |\n");
+   printf("|	MCCE can calculate residue pka, cofactor Em and       |\n");
+   printf("|	protein PI in protein-solvent systems, and more:      |\n");
+   printf("|							      |\n");
+   printf("|	 - Protein structural responses to changes in charge  |\n");
+   printf("|	 - Changes in charge state of ionizable residues due  |\n");
+   printf("|	   to structural changes in the protein		      |\n");
+   printf("|	 - The structural and ionization changes caused by    |\n");
+   printf("|	   changes in solution pH or Eh			      |\n");
+   printf("|	 - Find the location and stoichiometry of proton      |\n");
+   printf("|	   transfers coupled to electron transfer	      |\n");
+   printf("|	 - Make side chain rotomer packing predictions as     |\n");
+   printf("|	   a function of pH				      |\n");
+   printf("|							      |\n");
+   printf("|	For questions and help, visit                         |\n");
+   printf("|		https://sites.google.com/site/mccewiki/home   |\n");
+   printf("|		  October 2015, by MCCE Development Team      |\n");
+   printf("|							      |\n");
+   printf("|_________________________        ____________________________|\n");
+   printf("                          MCCE 3.0                          \n\n");
    printf("Last Updates:                                              \n");
-//   printf("   05/10, Step2, use relative energy to decide vdw clash   \n");
-   printf("   04/10, Added Pascal Comte's GA/SA algorithm for sidechain\n");
-   printf("          packing and sampling.     			      \n");
-   printf("   04/10, Added APBS electrostatic calculation option      \n");
-   printf("   08/08, Total pairwise is set to be 999.0 if vdw is 999.0\n");
-   printf("   08/02, step 3, zero interaction bug fixed               \n");
-   printf("   07/23, step 1 changes the policy on AltLoc backbone atoms\n");
-   printf("          now it is warning instead of fatal error.        \n");
-   printf("   06/22, step 1 recognizes FME and ACE as NTR caps        \n");
-   printf("   05/27, step 2 reduces rotation steps for surface residues\n");
-   printf("          The run.prm.default uses 0.25 SAS to decide suface\n");
-   printf("          residues.                                        \n");
-   printf("   05/04, Program doesn't stop at duplicate parameters,    \n");
-   printf("          it issues a warning message. This fixes the deadlock\n");
-   printf("          caused by multiple new cofactors                 \n");
-   printf("   04/24, Energy lookup table compressed, size is reduced  \n");
-   printf("          to about 1/12                                    \n");
-   printf("   04/24, Opp files are compressed. Temporary files are    \n");
-   printf("          stored under local directory.                    \n");
-   printf("   04/14, Step 2 rotamer pruning by pairwise corrected.    \n");
-   printf("   04/08, Step 2 repacking now ses better H bnd correction.\n");
-   printf("   01/26, Step 3 fixed incorrect errno message.            \n");
-   printf("   01/14, Step 3 dielectric boundary condition improved.   \n");
-   printf("   12/07, Step 3 dielectric boundary condition is corrected.\n");
-   printf("   11/17, pKa fitting writes \"pKa titration curve too sharp\"\n");
-   printf("          when occ jumps from 0.015 to 0.985.              \n");
-   printf("   11/17, Exposed conformer is marked as E in history.     \n");
-   printf("   11/02, Optional quick step 3 included.                  \n");
-   printf("   11/02, Pruning fuction is added at the end of step 2.   \n");
-   printf("          Parameter lines were added to run.prm.           \n");
-   printf("   10/29, Temporary gdbm files have file names.            \n");
-   printf("   10/11, Use biggest rxn energy from the last 3 delphi    \n");
-   printf("          focusing runs.                                   \n");
-   printf("===========================================================\n\n");
+   printf("July 2016, Yifan's monte carlo no longer needs step3_out.pdb directory (fixed)\n");
+   printf("July 2016, Removed PASCAL COMTE GENETIC ALGORITHM from this version\n");
    fflush(stdout);
+	
+	// Added by Jessica on Nov. 2015
+   char buf[LEN];
+   time_t curtime;
+   struct tm *loc_time;
 
+   //Getting current time of system
+   curtime = time (NULL);
+
+   // Converting current time to local time
+   loc_time = localtime (&curtime);
+
+   // Displaying date and time in standard format
+   printf("%s", asctime (loc_time));
    return;
 }
