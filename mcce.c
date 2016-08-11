54e8689c608e5e096507f70cde213cf26be84dac
mrgunner <marilyn.gunner@gmail.com>
2016-08-11 15:07:15 -0400

    added my name to mcce version output

--- mcce.c
+++ mcce.c
@@ -102,6 +102,13 @@ void welcome()
    printf("Last Updates:                                              \n");
    printf("July 2016, Yifan's monte carlo no longer needs step3_out.pdb directory (fixed)\n");
    printf("July 2016, Removed PASCAL COMTE GENETIC ALGORITHM from this version\n");
+
+
+
+
+
+
+   printf("Aug 2016:marilyn was here\n");
    fflush(stdout);
 	
 	// Added by Jessica on Nov. 2015
